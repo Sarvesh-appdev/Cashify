374f1f9d8c8290ee9e63818dbf6cd21899c1fcb0
sarvesh <sarveshthirums@gmail.com>
2025-05-28 23:13:30 -0300

    Established Firebase Connection

--- app/src/main/java/com/example/quickcash_summer15/MainActivity.java
+++ app/src/main/java/com/example/quickcash_summer15/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.quickcash_summer15;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Firebase;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase;
+
+    private DatabaseReference dbref;
+
+    private static final String FIREBASE_URL = "https://quickcash-summer15-default-rtdb.firebaseio.com/";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +36,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        connectFirebase();
+        writeToFirebase();
+        listentoDataChange();
+    }
+
+    private void listentoDataChange(){
+
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
+    }
+
+
+    private void connectFirebase(){
+        firebaseDatabase = FirebaseDatabase.getInstance(FIREBASE_URL);
+        dbref = firebaseDatabase.getReference("message");
+    }
+
+    private void writeToFirebase(){
+        dbref.setValue("Hello CSCI 3130");
     }
 }
